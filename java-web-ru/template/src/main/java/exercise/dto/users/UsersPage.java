54670dfa44fac0da2d7b623eea2404fda671f89c
@hexlet/cli <support@hexlet.io>
2024-11-14 23:02:18 +0100

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/dto/users/UsersPage.java
@@ -0,0 +1,19 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// BEGIN
+@Getter
+@AllArgsConstructor
+public class UsersPage {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
+// END
